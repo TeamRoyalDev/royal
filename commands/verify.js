999d5077726c042efa31224ed9888677d94b7ad2
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-17 10:15:54 -0700

    Create verify.js

---
+++ commands/verify.js
@@ -0,0 +1,16 @@
+const Discord = require("Discord.js");
+
+module.exports.run = async (bot,message, args) => {
+  if (!channelName.startsWith(`verify`)) return message.channel.send(`You can execute this command in here!`) 
+       .then(msg => {
+        msg.delete(5000)
+      })
+  if(message.member.hasPermission("VIEW_CHANNEL")) {
+    let role = member.guild.roles.find("name", "💰 Member");
+    member.addRole(role).catch(console.error);
+  }
+}
+
+module.exports.help = {
+  name: "verify"
+}
